c237806643c4380d00aee1fca32a0498b3a2b13d
Muhammad Awais <muhammad.awais@trilogy.com>
2022-04-21 11:01:22 +0000

    refactor: renaming stack

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,4 +1,4 @@
-import MyStack from "./MyStack";
+import BackendStack from "./BackendStack";
 import * as sst from "@serverless-stack/resources";
 
 export default function main(app: sst.App): void {
@@ -7,7 +7,7 @@ export default function main(app: sst.App): void {
     runtime: "nodejs14.x"
   });
 
-  new MyStack(app, "my-stack");
+  new BackendStack(app, "backend");
 
   // Add more stacks
 }
--- test/BackendStack.test.ts
+++ test/BackendStack.test.ts
@@ -1,11 +1,11 @@
 import { Template } from "aws-cdk-lib/assertions";
 import * as sst from "@serverless-stack/resources";
-import MyStack from "../stacks/MyStack";
+import BackendStack from "../stacks/BackendStack";
 
-test("Test Stack", () => {
+test("Test BackendStack", () => {
   const app = new sst.App();
   // WHEN
-  const stack = new MyStack(app, "test-stack");
+  const stack = new BackendStack(app, "test-stack");
   // THEN
   const template = Template.fromStack(stack);
   template.resourceCountIs("AWS::Lambda::Function", 1);
